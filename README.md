c3f19b5bdb801674e40151164ff96e550d55c49b
thejordanprice <thejordanprice@gmx.com>
2017-10-23 13:30:56 -0400

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # banhammer
+
 A small webapp built in JavaScript to parse log files (such as auth.log); then return copy/paste firewall rules to ban incoming traffic from said addresses on multiple routing/firewall platforms.
+
+### Intro
+
+    SSH attemps and what not, yadda yadda.. Block stuff.
+
+##### Screenshot
+
+![image](#)
+
+### Usage
+
+    Using this is fairly straight forward.
+
+##### Cisco iOS
+
+    Gotta do.
+
+##### Mikrotik RouterBoard
+
+    Gotta do.
+
+##### UFW
+
+    Gotta do.
+
+##### iptables
+
+    Gotta do.
+
+
+### License
+
+MIT
---
+++ src/banhammer.js
@@ -0,0 +1,113 @@
+'use strict';
+
+/**
+ *  Example input of files... generally found in /var/log/auth.log and such.
+ *
+ *  Oct 19 17:34:23 ubuntu sshd[1866]: Did not receive identification string from 10.0.0.0 
+ *  Oct 19 17:34:28 ubuntu sshd[1867]: Did not receive identification string from 10.0.0.0 
+ *  Eventually will be....
+ *  @param {blob} blob
+ *  @param {string} output
+ *  let blob = 'a text file that has a bunch of ip\'s in it';
+ *	let output = document.getElementById('div');
+ *  logsToBadGuys(blob, output)
+ */
+const dropZone = document.getElementById('drop-zone');
+const uploadForm = document.getElementById('upload-form');
+const arrayWindow = document.getElementById('array-window');
+const landingWindow = document.getElementById('landing-window');
+const typeSelector = document.getElementById('type-selector');
+const whitelistInput = document.getElementById('whitelist-input');
+const output = document.getElementById('output');
+
+const startMagic = function(files) {
+
+  for (let file in files) {
+    if (!files[file]) {
+      return;
+    };
+    
+    const reader = new FileReader();
+    let blob = new Blob([files[file]], {
+      type: "text/plain"
+    });
+    reader.onload = function() {
+    
+      let lines = this.result.split('\n');
+      let badAddresses = [];
+      for (let line = 0; line < lines.length; line++) {
+        const ipPattern = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/ig;
+        if (ipPattern.test(lines[line])) {
+          let string = lines[line].toString();
+          let matches = string.match(ipPattern);
+          for (let match in matches) {
+          	let whitelist = whitelistInput.value.split(',');
+            if (whitelist.indexOf(matches[match]) === -1) {
+              if (badAddresses.indexOf(matches[match]) === -1) {
+                badAddresses.push(matches[match]);
+              };
+            };
+          };
+        };
+      };
+      
+      landingWindow.style.display = 'none';
+      arrayWindow.style.display = 'flex';
+      
+      
+      if (typeSelector.value === 'Cisco iOS') {
+        for (let ip in badAddresses) {
+          let string = 'deny ip host ' + badAddresses[ip] + ' any log\n';
+          output.append(string);
+        };
+      };
+      if (typeSelector.value === 'Mikrotik RouterBoard') {
+        for (let ip in badAddresses) {
+          let string = '/ip firewall filter add action=drop chain=input comment=\'BANNED\' in-interface=ether1 src-address=' + badAddresses[ip] + '\n';
+          output.append(string);
+        };
+      };
+      if (typeSelector.value === 'UFW') {
+        for (let ip in badAddresses) {
+          let string = 'sudo ufw deny from ' + badAddresses[ip] + ' to any\n';
+          output.append(string);
+        };
+      };
+      
+      
+      
+    };
+    reader.readAsText(blob);
+  };
+};
+
+uploadForm.addEventListener('submit', function(event) {
+  let uploadFiles = document.getElementById('upload-files').files;
+  event.preventDefault();
+  startMagic(uploadFiles);
+});
+
+dropZone.ondrop = function(event) {
+  event.preventDefault();
+  this.className = 'upload-drop-zone';
+  startMagic(event.dataTransfer.files);
+};
+
+dropZone.ondragover = function() {
+  this.className = 'upload-drop-zone drop';
+  return false;
+};
+
+dropZone.ondragleave = function() {
+  this.className = 'upload-drop-zone';
+  return false;
+};
+
+document.getElementById('copy')
+.addEventListener('click', function(event) {
+  document.getElementById('output').select();
+  document.execCommand('copy');
+  if (output.value) {
+    output.innerHTML = 'Copied to clipboard.';
+  };
+});
