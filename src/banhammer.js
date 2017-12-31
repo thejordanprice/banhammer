0795b44e3d1549f5c88366fec4fa21014bc9f12f
Jordan Price <thejordanprice@gmx.com>
2017-12-31 02:33:02 -0500

    fixed cisco output

--- src/banhammer.js
+++ src/banhammer.js
@@ -22,66 +22,69 @@ const output = document.getElementById('output');
 
 const startMagic = function(files) {
 
-  for (let file in files) {
-    if (!files[file]) {
-      return;
-    };
-    
-    const reader = new FileReader();
-    let blob = new Blob([files[file]], {
-      type: "text/plain"
-    });
-    reader.onload = function() {
-    
-      let lines = this.result.split('\n');
-      let badAddresses = [];
-      for (let line = 0; line < lines.length; line++) {
-        const ipPattern = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/ig;
-        if (ipPattern.test(lines[line])) {
-          let string = lines[line].toString();
-          let matches = string.match(ipPattern);
-          for (let match in matches) {
-          	let whitelist = whitelistInput.value.split(',');
-            if (whitelist.indexOf(matches[match]) === -1) {
-              if (badAddresses.indexOf(matches[match]) === -1) {
-                badAddresses.push(matches[match]);
-              };
+  //  for (let file in files) {
+  //    if (!files[file]) {
+  //     return;
+  //    };
+  let file = 0;
+
+  const reader = new FileReader();
+  let blob = new Blob([files[file]], {
+    type: "text/plain"
+  });
+  reader.onload = function() {
+
+    let lines = this.result.split('\n');
+    let badAddresses = [];
+    for (let line = 0; line < lines.length; line++) {
+      const ipPattern = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/ig;
+      if (ipPattern.test(lines[line])) {
+        let string = lines[line].toString();
+        let matches = string.match(ipPattern);
+        for (let match in matches) {
+          let whitelist = whitelistInput.value.split(',');
+          if (whitelist.indexOf(matches[match]) === -1) {
+            if (badAddresses.indexOf(matches[match]) === -1) {
+              badAddresses.push(matches[match]);
             };
           };
         };
       };
-     
-      landingWindow.style.display = 'none';
-      arrayWindow.style.display = 'flex';
-      
-      if (typeSelector.value === 'Cisco iOS') {
-        for (let ip in badAddresses) {
-          let string = 'deny ip host ' + badAddresses[ip] + ' any log\n';
-          output.append(string);
-        };
-      };
-      if (typeSelector.value === 'iptables') {
-        for (let ip in badAddresses) {
-          let string = 'sudo iptables -A INPUT -s ' + badAddresses[ip] + ' -j DROP\n';
-          output.append(string);
-        };
-      };
-      if (typeSelector.value === 'Mikrotik RouterBoard') {
-        for (let ip in badAddresses) {
-          let string = '/ip firewall filter add action=drop chain=input comment=\'BANNED\' in-interface=ether1 src-address=' + badAddresses[ip] + '\n';
-          output.append(string);
-        };
-      };
-      if (typeSelector.value === 'UFW') {
-        for (let ip in badAddresses) {
-          let string = 'sudo ufw deny from ' + badAddresses[ip] + ' to any\n';
-          output.append(string);
-        };
-      };
-      
     };
-    reader.readAsText(blob);
+
+    landingWindow.style.display = 'none';
+    arrayWindow.style.display = 'flex';
+
+
+    if (typeSelector.value === 'Cisco iOS') {
+      output.append('ip access-list extended BLOCK\n');
+      output.append('permit icmp any any echo-reply\n');
+      output.append('permit icmp any any unreachable\n');
+      output.append('deny icmp any any log\n');
+      for (let ip in badAddresses) {
+        let string = 'deny ip host ' + badAddresses[ip] + ' any log\n';
+        output.append(string);
+      };
+      output.append('permit ip any any\n');
+    };
+    if (typeSelector.value === 'Mikrotik RouterBoard') {
+      for (let ip in badAddresses) {
+        let string = '/ip firewall filter add action=drop chain=input comment=\'BANNED\' in-interface=ether1 src-address=' + badAddresses[ip] + '\n';
+        output.append(string);
+      };
+    };
+    if (typeSelector.value === 'UFW') {
+      for (let ip in badAddresses) {
+        let string = 'sudo ufw deny from ' + badAddresses[ip] + ' to any\n';
+        output.append(string);
+      };
+    };
+
+
+
   };
+  reader.readAsText(blob);
+  // };
 };
 
 uploadForm.addEventListener('submit', function(event) {
@@ -107,10 +110,10 @@ dropZone.ondragleave = function() {
 };
 
 document.getElementById('copy')
-.addEventListener('click', function(event) {
-  document.getElementById('output').select();
-  document.execCommand('copy');
-  if (output.value) {
-    output.innerHTML = 'Copied to clipboard.';
-  };
-});
+  .addEventListener('click', function(event) {
+    document.getElementById('output').select();
+    document.execCommand('copy');
+    if (output.value) {
+      output.innerHTML = 'Copied to clipboard.';
+    };
+  });
